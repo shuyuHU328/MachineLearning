2dcfacfd73f7dfb72831805a0b2c693c43c35271
Hugo <1621740564@qq.com>
2023-10-16 22:01:43 +0800

    add Decision Tree with cart

---
+++ DecisionTree/cart.py
@@ -0,0 +1,97 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from graphviz import Digraph
+
+
+def min_gini(labels, attributes):
+    centrals = []
+    new_list = []
+    label_set = np.unique(labels)
+    for i in range(len(labels)):
+        new_list.append((labels[i], attributes[i]))
+    for i in range(len(attributes) - 1):
+        centrals.append((attributes[i] + attributes[i + 1]) / 2)
+    split_num = 0
+    min_gini_num = None
+    for num in centrals:
+        left = []
+        right = []
+        for i in range(len(new_list)):
+            if new_list[i][1] <= num:
+                left.append(new_list[i][0])
+            else:
+                right.append(new_list[i][0])
+        left_gini = 1
+        right_gini = 1
+        for label in label_set:
+            if len(left) != 0:
+                left_gini -= (left.count(label) / len(left)) ** 2
+            if len(right) != 0:
+                right_gini -= (right.count(label) / len(right)) ** 2
+        gini_now = left_gini * len(left) / len(attributes) + right_gini * len(right) / len(attributes)
+        if min_gini_num is None or min_gini_num > gini_now:
+            min_gini_num = gini_now
+            split_num = num
+    return min_gini_num, split_num
+
+
+class TreeNode:
+    def __init__(self, attributes, labels):
+        self.is_leaf = False
+        self.attributes = attributes
+        if len(set(labels)) == 1:
+            self.label = labels[0]
+            self.is_leaf = True
+        else:
+            properties = []
+            for i in range(attributes.shape[1]):
+                gini_num, split_num = min_gini(labels, attributes[:, i])
+                properties.append((i, gini_num, split_num))
+            index = np.argmin(list(map(lambda x: x[1], properties)))
+            self.prop = properties[index][0]
+            self.split_num = properties[index][2]
+            left_attr = []
+            left_labels = []
+            right_attr = []
+            right_labels = []
+            for i in range(len(attributes)):
+                if attributes[i][self.prop] <= self.split_num:
+                    left_attr.append(attributes[i])
+                    left_labels.append(labels[i])
+                else:
+                    right_attr.append(attributes[i])
+                    right_labels.append(labels[i])
+            self.left_node = TreeNode(np.array(left_attr), left_labels)
+            self.right_node = TreeNode(np.array(right_attr), right_labels)
+
+    def add_node(self, graph, labels, class_names):
+
+        graph.node(str(self.__hash__()), self.describe(labels, class_names), shape='box',
+                   style="filled" if self.is_leaf else None)
+        if not self.is_leaf:
+            self.left_node.add_node(graph, labels, class_names)
+            self.right_node.add_node(graph, labels, class_names)
+            graph.edge(str(self.__hash__()), str(self.left_node.__hash__()))
+            graph.edge(str(self.__hash__()), str(self.right_node.__hash__()))
+
+    def describe(self, labels, class_names):
+        description = ''
+        if self.is_leaf:
+            description += 'class = ' + class_names[self.label] + '\n'
+        else:
+            description += labels[self.prop] + ' <= ' + str(self.split_num) + '\n'
+        description += 'samples = ' + str(len(self.attributes))
+        return description
+
+
+if __name__ == '__main__':
+    iris = datasets.load_iris()
+    labels_name = iris.feature_names
+    target_name = iris.target_names
+    x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2)
+    root = TreeNode(np.array(x_train), y_train)
+    # 绘制树
+    g = Digraph('G', filename='decision_tree.gv')
+    root.add_node(g, labels_name, target_name)
+    g.view()
